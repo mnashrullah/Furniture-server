b56927f2e6e64068f0f3a31b6c10526453f52c65
Muhammad Nashrullah <muhammadnashrullah.it@gmail.com>
2019-04-10 13:48:23 +0700

    Update databaseConfig.js

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -1,8 +1,14 @@
 var mysql = require ('mysql');
+// var dbconnect = mysql. createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "dbfurniture"
+// });
 var dbconnect = mysql. createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "dbfurniture"
+    host: "jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "xjm8i11lkd1bi9sz",
+    password: "hzhq5zbzgnh3gyr7",
+    database: "yr75klicvxjvw39i"
 });
 module.exports = dbconnect
