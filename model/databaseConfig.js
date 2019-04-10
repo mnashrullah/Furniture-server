a2e2a0c9100a45da7d3968120ad509fd37b82142
Muhammad Nashrullah <muhammadnashrullah.it@gmail.com>
2019-04-10 13:55:15 +0700

    Update databaseConfig.js

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -5,7 +5,7 @@ var mysql = require ('mysql');
 //     password: "",
 //     database: "dbfurniture"
 // });
-var dbconnect = mysql. createPool({
+var dbconnect = mysql.createPool({
     host: "jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "xjm8i11lkd1bi9sz",
     password: "hzhq5zbzgnh3gyr7",
