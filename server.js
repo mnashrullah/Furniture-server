571b296d48fb6fe68047991f94d27ece72b19b9d
Muhammad Nashrullah <muhammadnashrullah.it@gmail.com>
2019-04-10 14:20:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = require('./controller/app.js');
 //     var port = server.address().port;
 //     console.log('Web App Hosted at http://localhost:%s', port);
 // })
-var server = app.listen('https://ionic-furniture-server.herokuapp.com/', function(){
-    var port = server.address().port;
-    console.log('Web App Hosted at http://localhost:%s', port);
+var server = app.listen(function(){
+    var port = 'https://ionic-furniture-server.herokuapp.com/';
+    console.log('Web App Hosted at %s', port);
 })
